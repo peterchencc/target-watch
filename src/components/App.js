b04be42f27686af30492bf4a1077568a5152a61d
peterchencc <peterchencc@gmail.com>
2020-08-15 18:09:35 +0800

    add SignUp SignIn and modify router

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,15 @@ import cheerio from 'cheerio'
 import moment from 'moment'
 import { auth, database } from '../base'
 import Symbol from './Symbol'
+import SignUp from './SignUp'
+import SignIn from './SignIn'
+import NotFound from './NotFound'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 class App extends Component {
   state = {
+    uid: null,
+    displayName: null,
     symbols: {},
     currentSymbol: '',
   }
@@ -176,8 +182,22 @@ class App extends Component {
   componentDidMount() {
     document.body.classList.add('bg-gray-100')
 
-    console.log('componentDidMount', this)
-    console.log('auth', auth)
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log('User is signed in.')
+        console.log('user', user)
+        // this.authHandler({ user })
+
+        // 1. set the state of the user to reflect the current user
+        this.setState({
+          uid: user.uid,
+          displayName: user.displayName,
+        })
+      } else {
+        console.log('No user is signed in.')
+      }
+    })
+
     database
       .ref('symbols')
       .orderByChild('timestamp')
@@ -191,43 +211,90 @@ class App extends Component {
       })
   }
 
+  logout = async () => {
+    console.log('Logging out!')
+    await auth.signOut()
+    this.setState({ uid: null })
+  }
+
   render() {
     return (
-      <div className="container mx-auto px-2">
-        <h1 className="leading-relaxed">
-          <span role="img">📈</span> Target Watch
-        </h1>
+      <Router>
+        <div className="container mx-auto px-2">
+          <header className="flex justify-between">
+            <Link to="/">
+              <h1 className="leading-relaxed">
+                <span role="img">📈</span> Target Watch
+              </h1>
+            </Link>
+            <nav className="">
+              {this.state.uid ? (
+                <div className="flex">
+                  <div className="p-2 mx-2">Hi, {this.state.displayName}</div>
+                  <button
+                    className="p-2"
+                    onClick={() => {
+                      this.logout()
+                    }}
+                  >
+                    Sign out
+                  </button>
+                </div>
+              ) : (
+                <div className="flex">
+                  <Link className="p-2 mx-2" to="/signup">
+                    sign up
+                  </Link>
+                  <Link className="p-2 mx-2" to="/signin">
+                    sign in
+                  </Link>
+                </div>
+              )}
+            </nav>
+          </header>
 
-        <form onSubmit={this.handleSubmit}>
-          <input
-            id="new-symbol"
-            type="text"
-            placeholder="Symbol"
-            onChange={this.handleChange}
-            value={this.state.symbol}
-            style={{ textTransform: 'uppercase' }}
-            className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none"
-          />
-          <button className="btn btn-blue">Add Symbol </button>
-        </form>
+          <Switch>
+            <Route exact path="/">
+              <form onSubmit={this.handleSubmit}>
+                <input
+                  id="new-symbol"
+                  type="text"
+                  placeholder="Symbol"
+                  onChange={this.handleChange}
+                  value={this.state.symbol}
+                  style={{ textTransform: 'uppercase' }}
+                  className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+                />
+                <button className="btn btn-blue">Add Symbol </button>
+              </form>
 
-        <div className="">
-          <section className="my-8">
-            <div className="text-xl py-2">Recent Search</div>
-            <div className=" py-4 border-t border-gray-400 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-              {Object.keys(this.state.symbols)
-                .reverse()
-                .map((key) => (
-                  <Symbol
-                    key={key}
-                    index={key}
-                    details={this.state.symbols[key]}
-                  />
-                ))}
-            </div>
-          </section>
+              <div className="">
+                <section className="my-8">
+                  <div className="text-xl py-2">Recent Search</div>
+                  <div className=" py-4 border-t border-gray-400 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+                    {Object.keys(this.state.symbols)
+                      .reverse()
+                      .map((key) => (
+                        <Symbol
+                          key={key}
+                          index={key}
+                          details={this.state.symbols[key]}
+                        />
+                      ))}
+                  </div>
+                </section>
+              </div>
+            </Route>
+            <Route exact path="/signup">
+              <SignUp />
+            </Route>
+            <Route exact path="/signin">
+              <SignIn />
+            </Route>
+            <Route component={NotFound} />
+          </Switch>
         </div>
-      </div>
+      </Router>
     )
   }
 }
