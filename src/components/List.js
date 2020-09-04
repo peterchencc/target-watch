3f7df5714863a33547b7998262634220516484c4
peterchencc <peterchencc@gmail.com>
2020-09-04 17:39:43 +0800

    add sortable feature on List

--- src/components/List.js
+++ src/components/List.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react'
 import { firestore } from '../base'
 import Symbol from './Symbol'
 import AddSymbolForm from './AddSymbolForm'
+import { SortableContainer, SortableElement } from 'react-sortable-hoc'
+import arrayMove from 'array-move'
 
 const List = (props) => {
   const { list, symbols } = props
   const { id, title } = list
   const [selectedSymbols, setSelectedSymbols] = useState(list.symbols)
+  const [sortable, setSortable] = useState(false)
 
   const addSymbol = (symbol) => {
     props.getSymbol(symbol.name.toUpperCase())
@@ -36,6 +39,48 @@ const List = (props) => {
     }
   }
 
+  const SortableItem = SortableElement(({ value, index }) => (
+    <Symbol
+      key={index}
+      name={value.name}
+      data={symbols[value.name]}
+      getSymbol={props.getSymbol}
+    />
+  ))
+
+  const SortableList = SortableContainer(({ items }) => {
+    return (
+      <div className="py-4 block relative clearfix">
+        {items.map((value, index) => (
+          <SortableItem
+            disabled={sortable}
+            key={`item-${index}`}
+            index={index}
+            value={value}
+          />
+        ))}
+      </div>
+    )
+  })
+
+  const onSortEnd = ({ oldIndex, newIndex }) => {
+    const sortedList = arrayMove(selectedSymbols, oldIndex, newIndex)
+    setSelectedSymbols(sortedList)
+
+    const listRef = firestore.collection('watchlists').doc(id)
+    listRef
+      .update({
+        symbols: [...sortedList],
+      })
+      .then(function () {
+        console.log('Document successfully updated!')
+      })
+      .catch(function (error) {
+        // The document probably doesn't exist.
+        console.error('Error updating document: ', error)
+      })
+  }
+
   return (
     <div className="">
       <section className="my-8">
@@ -43,6 +88,13 @@ const List = (props) => {
           <div className="text-xl py-2">{title}</div>
           <AddSymbolForm addSymbol={addSymbol}></AddSymbolForm>
           <button
+            className="btn border ml-2"
+            onClick={() => setSortable(!sortable)}
+          >
+            {sortable ? 'Unsortable' : 'Sortable'}
+          </button>
+          <button
+            className="btn border ml-2"
             onClick={() => {
               if (window.confirm('Are you sure to delete this list?')) {
                 props.deleteList(id)
@@ -54,17 +106,13 @@ const List = (props) => {
         </div>
 
         {selectedSymbols.length > 0 ? (
-          <div className="py-4 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-            {selectedSymbols
-              .sort((a, b) => b.timestamp - a.timestamp)
-              .map((symbol) => (
-                <Symbol
-                  key={symbol.name}
-                  name={symbol.name}
-                  data={symbols[symbol.name]}
-                  getSymbol={props.getSymbol}
-                />
-              ))}
+          //grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4
+          <div className="py-4">
+            <SortableList
+              axis="xy"
+              items={selectedSymbols}
+              onSortEnd={onSortEnd}
+            />
           </div>
         ) : (
           <div className="flex justify-center align-middle py-6">
